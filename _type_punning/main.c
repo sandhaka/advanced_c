17eabb220c84c55af622562e54e6547d9178f1aa
Matteo Tosato <matteo.tosato7@gmail.com>
2025-07-30 14:17:05 +0200

    type punning

---
+++ _type_punning/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdint.h>
+
+typedef union {
+    float f;
+    unit32_t u;
+} Data;
+
+int main() {
+    Data data;
+    data.f = 3.14f;
+    data.u = *(uint32_t*)&data.f;
+
+    printf("Float: %f\n", data.f);
+    printf("Bits: 0x%08X\n", data.u);
+
+    return 0;
+}
